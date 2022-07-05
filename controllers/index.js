e96b14192e3ce2200dc6343bb2f478f093ab3028
Kauã Maximino <kaua_maximino@outlook.com>
2022-07-05 18:30:43 -0300

    feat: camada de controllers do req 8 ok

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const getAllProducts = require('./getAllProducts');
 const getProductById = require('./getProductById');
 const insertProduct = require('./insertProduct');
 const listSales = require('./listSales');
+const listSalesId = require('./listSalesId');
 
 module.exports = {
   getAllProducts,
   getProductById,
   insertProduct,
   listSales,
+  listSalesId,
 };
---
+++ controllers/listSalesId.js
@@ -0,0 +1,14 @@
+const services = require('../services');
+
+const listSalesId = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const salesId = await services.listSalesId(id);
+      console.log(`${salesId} controllers`);
+    return response.status(200).json(salesId);
+  } catch (error) {
+    return response.status(404).json({ message: error.message });
+  }
+};
+
+module.exports = listSalesId;
