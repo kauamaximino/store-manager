e7d79ffcb495a15e99a0fdf5c11ce3389d355838
Kauã Maximino <kaua_maximino@outlook.com>
2022-07-05 12:28:41 -0300

    feat: criando validações para o requisito 4

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./app');
 require('dotenv').config();
 
 const controllers = require('./controllers');
+const middlewares = require('./middlewares');
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
@@ -9,7 +10,7 @@ app.get('/products/:id', controllers.getProductById);
 
 app.get('/products', controllers.getAllProducts);
 
-app.post('/products', controllers.insertProduct);
+app.post('/products', middlewares.validateName, controllers.insertProduct);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
